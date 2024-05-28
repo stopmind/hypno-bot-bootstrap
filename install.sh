5650e5aea16d08bb7483b8aaae3a2c3ddeb73cfc
stopmind <smaxim120@gmail.com>
2024-05-28 14:03:31 +0300

    README + install script fix

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 #!/bin/env sh
 
-hypno_path = "~/hypno-bot"
-read -p "Where do you want to store HypnoBot:" hypno_path
+HYPNO=""
+read -p "Where do you want to store HypnoBot? (full path): " HYPNO
 
-mkdir $hypno_path
-cd $hypno_path
+mkdir -p $HYPNO
+cd $HYPNO
 
 git clone https://github.com/stopmind/hypno-bot-bootstrap
-mv -r hypno-bot-bootstrap bootstrap
+mv hypno-bot-bootstrap bootstrap
 
 chmod +x ./bootstrap/main.py ./bootstrap/runner.py
 ln -s ./bootstrap/main.py bot
